f0602f7c656cb2cc8e6626f767e1f20c93395113
kazuma_w <conporuno.jp@docomo.ne.jp>
2017-11-07 19:26:49 +0900

    閲覧画面追加

---
+++ app/src/main/java/com/example/connect2017withb/shareapp/HueAdapter.java
@@ -0,0 +1,85 @@
+package com.example.connect2017withb.shareapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.widget.Button;
+
+/**
+ * Created by Administrator on 2017/11/06.
+ */
+
+public class HueAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+    private String[] mHueArray = {
+            "fukei_mura", "リカチャンキャッスル", "磐梯熱海温泉",
+            "郡山市ふれあい科学館", "開成山公園","阿武隈洞","阿武隈洞","阿武隈洞","阿武隈洞","阿武隈洞","阿武隈洞","阿武隈洞","阿武隈洞","阿武隈洞","阿武隈洞"
+    };
+    private Integer[] mHueIdArray = {
+            R.drawable.fukei_mura,
+            R.drawable.rikachan,
+            R.drawable.bandaia,
+            R.drawable.koriyama_hureai,
+            R.drawable.kaiseizan_park,
+            R.drawable.abukumadou,
+            R.drawable.abukumadou,
+            R.drawable.abukumadou,
+            R.drawable.abukumadou,
+            R.drawable.abukumadou,
+            R.drawable.abukumadou,
+            R.drawable.abukumadou,
+            R.drawable.abukumadou,
+            R.drawable.abukumadou,
+            R.drawable.abukumadou,
+    };
+    private static class ViewHolder {
+        public ImageView hueImageView;
+        public TextView  hueTextView;
+    }
+
+    public HueAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+
+    public int getCount() {
+        return mHueArray.length;
+    }
+
+    public Object getItem(int position) {
+        return mHueArray[position];
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(R.layout.grid_item_hue, null);
+            holder = new ViewHolder();
+            holder.hueImageView = (ImageView)convertView.findViewById(R.id.hue_imageview);
+            holder.hueTextView = (TextView)convertView.findViewById(R.id.hue_textview);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+        holder.hueImageView.setImageResource(mHueIdArray[position]);
+        holder.hueTextView.setText(mHueArray[position]);
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/connect2017withb/shareapp/MainActivity.java
+++ app/src/main/java/com/example/connect2017withb/shareapp/MainActivity.java
@@ -1,54 +1,27 @@
 package com.example.connect2017withb.shareapp;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.view.View.OnClickListener;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BaseAdapter adapter;
-
-    // 場所名
-    private static final String[] place = {
-            "いろはにほへといろはにほへといろはにほへといろはにほへといろはにほへといろはにほへといろはにほへといろはにほへと",
-            "ちりぬるをちりぬるをちりぬるをちりぬるをちりぬるをちりぬるをちりぬるをちりぬるをちりぬるをちりぬるをちりぬるをちりぬるを",
-            "寿限無寿限寿限無寿限寿限無寿限寿限無寿限寿限無寿限寿限無寿限寿限無寿限寿限無寿限寿限無寿限寿限無寿限寿限無寿限寿限無寿限寿限無寿限",
-            "誰だお前",
-            "隣の客はよく柿食う客だ隣の客はよく柿食う客だ隣の客はよく柿食う客だ隣の客はよく柿食う客だ隣の客はよく柿食う客だ隣の客はよく柿食う客だ隣の客はよく柿食う客だ隣の客はよく柿食う客だ隣の客はよく柿食う客だ",
-            "がががががががががががが",
-            "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-            "AKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAKAK"
-
-    };
-
-    // サムネイル
-    private static final int[] photos = {
-            R.drawable.fukei_mura,
-            R.drawable.fukei_mura,
-            R.drawable.fukei_mura,
-            R.drawable.fukei_mura,
-            R.drawable.fukei_mura,
-            R.drawable.fukei_mura,
-            R.drawable.fukei_mura,
-            R.drawable.fukei_mura
-    };
+    private TextView fukei_mura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ListViewのインスタンスを生成
-        ListView listView = findViewById(R.id.list_view);
-
-        // BaseAdapter を継承したadapterのインスタンスを生成
-        // レイアウトファイル list.xml を activity_main.xml に
-        // inflate するためにadapterに引数として渡す
-        adapter = new ListViewAdapter(this.getApplicationContext(),
-                R.layout.list, place, photos);
-
-        // ListViewにadapterをセット
-        listView.setAdapter(adapter);
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new HueAdapter(this));
 
     }
 }
