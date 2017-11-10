3a1eceb51d3e1e0c341841d5ec1d5ca8ca135280
jun_hasimoto <jun_hasimoto@fcs.co.jp>
2017-11-10 17:04:04 +0900

    タグ切り替え

--- app/src/main/java/com/example/connect2017withb/shareapp/MainActivity.java
+++ app/src/main/java/com/example/connect2017withb/shareapp/MainActivity.java
@@ -34,15 +34,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        TextView hue_tv= (TextView)findViewById(R.id.text);
-        hue_tv.setClickable(true);
-        hue_tv.setOnClickListener(new View.OnClickListener() {
-
+        Button taberuButton = (Button) findViewById(R.id.button);
+        // ボタンに OnClickListener インターフェースを実装する
+        taberuButton.setOnClickListener(new View.OnClickListener() {
             // クリック時に呼ばれるメソッド
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MoreActivity.class);
-                startActivity(intent);
+                TextView hue_tv= (TextView)findViewById(R.id.text);
+                hue_tv.setText("食べる");
+            }
+        });
+        Button asobuButton = (Button) findViewById(R.id.button4);
+        // ボタンに OnClickListener インターフェースを実装する
+        asobuButton.setOnClickListener(new View.OnClickListener() {
+            // クリック時に呼ばれるメソッド
+            @Override
+            public void onClick(View view) {
+                TextView hue_tv= (TextView)findViewById(R.id.text);
+                hue_tv.setText("遊ぶ");
+            }
+        });
+        Button miruButton = (Button) findViewById(R.id.button5);
+        // ボタンに OnClickListener インターフェースを実装する
+        miruButton.setOnClickListener(new View.OnClickListener() {
+            // クリック時に呼ばれるメソッド
+            @Override
+            public void onClick(View view) {
+                TextView hue_tv= (TextView)findViewById(R.id.text);
+                hue_tv.setText("観る");
+            }
+        });
+        Button tomaruButton = (Button) findViewById(R.id.button6);
+        // ボタンに OnClickListener インターフェースを実装する
+        tomaruButton.setOnClickListener(new View.OnClickListener() {
+            // クリック時に呼ばれるメソッド
+            @Override
+            public void onClick(View view) {
+                TextView hue_tv= (TextView)findViewById(R.id.text);
+                hue_tv.setText("泊まる");
             }
         });
 
